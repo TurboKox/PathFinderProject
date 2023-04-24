d84abcb7f4ab69b0d1922418233d995eac43be96
TurboKox <szymonieknisa@gmail.com>
2023-04-24 23:04:01 +0200

    Added a lot of stuff. Implemented BFS, added more visual effects and fixed some bugs. Basically finished the whole project.

--- Project/MainWindow.xaml.cs
+++ Project/MainWindow.xaml.cs
@@ -21,7 +21,9 @@ namespace Project
         {
             InitializeComponent();
 
-            GenerateTiles(10, 10);
+            GenerateTiles(xCount, yCount);
+            SetCell("Start", xStartPos, yStartPos);
+            SetCell("Destination", xDestPos, yDestPos);
         }
 
         private void GenerateTiles(int xCount, int yCount)
@@ -43,14 +45,15 @@ namespace Project
                 MainGrid.ColumnDefinitions.Add(columnDef);
             }
 
-            for (int i = 0; i < xCount; i++)
+            for (int i = 0; i < yCount; i++)
             {
-                for (int j = 0; j < yCount; j++)
+                for (int j = 0; j < xCount; j++)
                 {
                     Rectangle rectangle = new Rectangle();
                     rectangle.Fill = new SolidColorBrush(Color.FromRgb(255, 255, 255));
                     rectangle.Stroke = new SolidColorBrush(Color.FromRgb(150, 150, 150));
                     rectangle.Name = "Free";
+                    
                     m_Rectangles.Add(rectangle);
                     Grid.SetRow(rectangle, i);
                     Grid.SetColumn(rectangle, j);
@@ -72,6 +75,8 @@ namespace Project
                         cell.Fill = new SolidColorBrush(Color.FromRgb(0, 200, 0));
                         cell.Name = "Start";
                         m_IsStartSet = true;
+
+                        m_StartIndex = GetRectangleIndex(cell);
                     }
                 }
                 else if (m_CurrentAction == Action.SET_DESTINATION && !m_IsDestinationSet)
@@ -81,11 +86,13 @@ namespace Project
                         cell.Fill = new SolidColorBrush(Color.FromRgb(200, 0, 0));
                         cell.Name = "Destination";
                         m_IsDestinationSet = true;
+
+                        m_DestIndex = GetRectangleIndex(cell);
                     }
                 }
                 else
                 {
-                    if (cell.Name == "Free")
+                    if (cell.Name == "Free" || cell.Name == "Path")
                     {
                         cell.Fill = new SolidColorBrush(Color.FromRgb(100, 100, 100));
                         cell.Name = "Wall";
@@ -99,9 +106,61 @@ namespace Project
             }
         }
 
-        private void ClearCell(string cellType)
+        private int GetRectangleIndex(Rectangle rect)
         {
-            for (int i = 0; i < 10 * 10; i++)
+            for (int i = 0; i < m_Rectangles.Count; i++)
+            {
+                if (rect == m_Rectangles[i])
+                    return i;
+            }
+
+            return -1;
+        }
+
+        private void UpdateAdjecentList()
+        {
+            graph.ClearAdjecencyList();
+            for (int i = 0; i < yCount; i++)
+            {
+                for (int j = 0; j < xCount; j++)
+                {
+                    // sprawdz lewo
+                    if (j - 1 >= 0 && m_Rectangles[(j - 1) + i * yCount].Name != "Wall")
+                        graph.AddEdge(j + i * yCount, (j - 1) + i * yCount);
+
+                    //sprawdz prawo
+                    if (j + 1 < xCount && m_Rectangles[(j + 1) + i * yCount].Name != "Wall")
+                        graph.AddEdge(j + i * yCount, (j + 1) + i * yCount);
+
+                    // sprawdz gore
+                    if (i - 1 >= 0 && m_Rectangles[j + (i - 1) * yCount].Name != "Wall")
+                        graph.AddEdge(j + i * yCount, j + (i - 1) * yCount);
+
+                    // sprawdz dol
+                    if (i + 1 < yCount && m_Rectangles[j + (i + 1) * yCount].Name != "Wall")
+                        graph.AddEdge(j + i * yCount, j + (i + 1) * yCount);
+                }
+            }
+        }
+
+        private void SetCell(string cellType, int xCellIndex, int yCellIndex)
+        {
+            m_Rectangles[xCellIndex + yCellIndex * yCount].Name = cellType;
+            if (cellType == "Start")
+            {
+                m_Rectangles[xCellIndex + yCellIndex * yCount].Fill = new SolidColorBrush(Color.FromRgb(0, 200, 0));
+                m_IsStartSet = true;
+            }
+            else if (cellType == "Destination")
+            {
+                m_Rectangles[xCellIndex + yCellIndex * yCount].Fill = new SolidColorBrush(Color.FromRgb(200, 0, 0));
+                m_IsDestinationSet = true;
+            }
+        }
+
+        private void ClearGivenCell(string cellType)
+        {
+            for (int i = 0; i < xCount * yCount; i++)
             {
                 if (m_Rectangles[i].Name == cellType)
                 {
@@ -113,24 +172,126 @@ namespace Project
 
         private void SetStartPositionBtn_Click(object sender, RoutedEventArgs e)
         {
+            ClearCurrentPath();
             m_CurrentAction = Action.SET_START;
             if (m_IsStartSet)
-                ClearCell("Start");
+                ClearGivenCell("Start");
+
             m_IsStartSet = false;
         }
 
         private void SetDestinationBtn_Click(object sender, RoutedEventArgs e)
         {
+            ClearCurrentPath();
             m_CurrentAction = Action.SET_DESTINATION;
             if (m_IsDestinationSet)
-                ClearCell("Destination");
+                ClearGivenCell("Destination");
+
             m_IsDestinationSet = false;
         }
 
         private void FindPathBtn_Click(object sender, RoutedEventArgs e)
         {
-            m_Graph.FindShortestPath(3, 5);
+            UpdateAdjecentList();
+            ClearCurrentPath();
+            graph.BFS(m_StartIndex, m_DestIndex, ref m_Rectangles);
         }
+        private void ClearCurrentPath()
+        {
+            for (int i = 0; i < m_Rectangles.Count; i++)
+            {
+                if (m_Rectangles[i].Name == "Path")
+                    m_Rectangles[i].Fill = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+            }
+                
+        }
+
+        class Graph
+        {
+            public Graph(int verticiesCount)
+            {
+                m_AdjecencyList = new LinkedList<int>[verticiesCount];
+                V = verticiesCount;
+
+                for (int i = 0; i < m_AdjecencyList.Length; i++)
+                    m_AdjecencyList[i] = new LinkedList<int>();
+
+            }
+
+            public void ClearAdjecencyList()
+            {
+                m_AdjecencyList = new LinkedList<int>[V];
+
+                for (int i = 0; i < m_AdjecencyList.Length; i++)
+                    m_AdjecencyList[i] = new LinkedList<int>();
+            }
+
+            public void AddEdge(int v, int w)
+            {
+                m_AdjecencyList[v].AddLast(w);
+            }
+
+            public void BFS(int start, int destination, ref List<Rectangle> rectArr)
+            {
+                int[] parent = new int[V];
+                for (int i = 0; i < V; i++)
+                    parent[i] = -1;
+                bool[] visited = new bool[V];
+                for (int i = 0; i < V; i++)
+                    visited[i] = false;
+
+                LinkedList<int> queue = new LinkedList<int>();
+
+                visited[start] = true;
+                queue.AddLast(start);
+
+                while (queue.Any())
+                {
+                    int current = queue.First();
+                    if (current == destination)
+                        break;
+
+                    queue.RemoveFirst();
+
+                    LinkedList<int> list = m_AdjecencyList[current];
+
+                    foreach (var val in list)
+                    {
+                        if (!visited[val])
+                        {
+                            visited[val] = true;
+                            parent[val] = current;
+                            queue.AddLast(val);
+                        }
+                    }
+                }
+
+                List<int> path = new List<int>();
+                int curr = destination;
+                while (curr != -1)
+                {
+                    path.Add(curr);
+                    curr = parent[curr];
+                }
+                path.Reverse();
+
+                foreach (var val in path)
+                {
+                    if (val != start && val != destination)
+                    {
+                        rectArr[val].Fill = new SolidColorBrush(Color.FromRgb(255, 255, 0));
+                        rectArr[val].Name = "Path";
+                    }
+                }
+            }
+
+            private int V;
+            private LinkedList<int>[] m_AdjecencyList;
+        }
+
+        private static int xCount = 20, yCount = 20;
+        private static int xStartPos = 2, yStartPos = 3;
+        private static int xDestPos = 7, yDestPos = 4;
 
         private int m_ViewportWidth = 600;
         private int m_ViewportHeight = 600;
@@ -141,8 +302,10 @@ namespace Project
         {
             SET_START, SET_DESTINATION
         }
+
         private Action m_CurrentAction = (Action)3;
-        
-        private Graph m_Graph = new Graph();
+        private Graph graph = new Graph(xCount * yCount);
+        private int m_StartIndex = xStartPos + yStartPos * yCount;
+        private int m_DestIndex = xDestPos + yDestPos * yCount;
     }
 }
